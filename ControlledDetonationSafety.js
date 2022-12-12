1b7caf410ebfc110b9ec4837eb881df013e029cd
carloso0114 <carlossospinag@gmail.com>
2022-12-11 19:52:15 -0500

    add semicolons

--- ControlledDetonationSafety.js
+++ ControlledDetonationSafety.js
@@ -1,57 +1,57 @@
 function safe_mine_field(mine_field) {
   if (mine_field.length === 0) {
-    return []
-  } 
+    return [];
+  };
   const mine = 'M';
   const tree = 'T';
-  let copyArray = [...mine_field]
+  let copyArray = [...mine_field];
   function addCharges (array, x, y) {
     const charge = 'C';
      for (let i = y; i >= 0; i--) {
       if (array[x][i] === tree) {
-        break
+        break;
       } else if (array[x][i] === '.') {
-        array[x][i] = charge
+        array[x][i] = charge;
       }
     }
     for (let i = y; i < array[x].length; i++) {
       if (array[x][i] === tree) {
-        break
+        break;
       } else if (array[x][i] === '.') {
-        array[x][i] = charge
-      }
-    }
+        array[x][i] = charge;
+      };
+    };
     for (let j = x; j >= 0; j--) {
       if (array[j][y] === tree) {
-        break
+        break;
       } else if (array[j][y] !== mine) {
-        array[j][y] = charge
-      }
-    }
+        array[j][y] = charge;
+      };
+    };
     for (let j = x; j < array.length; j++) {
       if (array[j][y] === tree) {
-        break
+        break;
       } else if (array[j][y] !== mine) {
-        array[j][y] = charge
-      }
-    }
-  }
+        array[j][y] = charge;
+      };
+    };
+  };
   mine_field.forEach((element1, index1) => {
     element1.forEach((element2, index2) => {
       if (element2 === mine) {
         addCharges(copyArray, index1 ,index2)
-      }
-    })
+      };
+    });
   });
-  const result = []
+  const result = [];
   copyArray.forEach((element1, index1) => {
     element1.forEach((element2, index2) => {
       if (element2 === '.') {
-        result.push([index1 ,index2])
-      }
-    })
+        result.push([index1 ,index2]);
+      };
+    });
   });
-  return result
+  return result;
 }
 
 var field = [
@@ -62,4 +62,4 @@ var field = [
   ['.', '.', '.', '.', '.']
 ];
 
-console.log(safe_mine_field(field))
+console.log(safe_mine_field(field));
